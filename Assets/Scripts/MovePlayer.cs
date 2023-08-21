614b6747d445da3e6ad1762ef88c4bd2d924c999
Ezerwald <139245703+Ezerwald@users.noreply.github.com>
2023-08-21 21:33:04 +0300

    Player Movement Code Update
    1. Rebuilt code of player's movement:
    - better variables' names
    - improved incapsulation
    - code optimizated
    2. Adjusted player's collision box, jump height and movement speed

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -11,46 +11,67 @@ public class MovePlayer : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] float GroundCheckRadius = 0.2f;
 
-    float horizontal;
-    bool isFacingRight = true;
-    float speed_x;
-    float speed_y;
-    void Update()
+    private float horizontalInput;
+    private bool jumpInput;
+    private bool isFacingRight = true;
+    private float currentHorizontalSpeed;
+    private float currentVerticalSpeed;
+
+    private void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        speed_x = rb.velocity.x;
-        speed_y = rb.velocity.y;
-
-        if (Input.GetButtonDown("Jump") && IsGrounded())
-        {
-            rb.velocity = new Vector2(speed_x, jumpingPower);
-        }
-
-        if (Input.GetButtonUp("Jump") && speed_y > 0f)
-        {
-            rb.velocity = new Vector2(speed_x, speed_y * 0.5f);
-        }
-
-        Flip();
+        HandleInput();
+        HandleJump();
+        UpdateAnimation();
+        FlipIfNeeded();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * moveSpeed, speed_y);
-        animator.SetFloat("Speed_Horizontal", Mathf.Abs(horizontal));
-        animator.SetFloat("Speed_Vertical", speed_y);
+        currentHorizontalSpeed = rb.velocity.x;
+        currentVerticalSpeed = rb.velocity.y;
+        HandleMovement(); 
+    }
+
+    private void HandleInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void UpdateAnimation()
+    {
+        animator.SetFloat("Speed_Horizontal", Mathf.Abs(horizontalInput));
+        animator.SetFloat("Speed_Vertical", currentVerticalSpeed);
         animator.SetBool("Is_Grounded", IsGrounded());
     }
 
-    bool IsGrounded()
+    private void HandleMovement()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, currentVerticalSpeed);
     }
 
-    void Flip()
+    private void HandleJump()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(currentHorizontalSpeed, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && currentVerticalSpeed > 0f)
+        {
+            rb.velocity = new Vector2(currentHorizontalSpeed, currentVerticalSpeed * 0.5f);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, GroundCheckRadius, groundLayer);
+    }
+
+    private void FlipIfNeeded()
+    {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
